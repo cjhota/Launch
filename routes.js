b503ad02ffd7ee49e6a8570e1d2af5cd30322017
José Ribeiro <josekcarvalho116@gmail.com>
2021-07-09 19:02:06 -0300

    construção da pagina teachers

---
+++ routes.js
@@ -0,0 +1,33 @@
+const express = require("express")
+
+const routes = express.Router()
+
+routes.get("/", function(req, res){
+    return res.redirect("/teachers")
+})
+
+routes.get("/teachers", function(req, res){
+    return res.render("teachers/index")
+})
+
+routes.get("/teachers/create", function(req, res) {
+    return res.render("teachers/create")
+})
+
+routes.post("/teachers", function(req, res) {
+
+    const keys = Object.keys(req.body)
+
+    for(key of keys) {
+        if (req.body[key] = "") {
+            return res.send('Please, fill all fields')
+        }
+    }
+    return
+})
+
+routes.get("/students", function(req, res){
+    return res.send("students")
+})
+
+module.exports = routes
